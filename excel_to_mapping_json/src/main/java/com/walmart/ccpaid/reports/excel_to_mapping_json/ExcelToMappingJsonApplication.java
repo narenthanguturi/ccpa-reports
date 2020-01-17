6ec83af0cadfe2c060e2b7026af61ee537a55b06
narenthanguturi <35933922+narenthanguturi@users.noreply.github.com>
2020-01-17 11:48:21 -0600

    Add files via upload

---
+++ excel_to_mapping_json/src/main/java/com/walmart/ccpaid/reports/excel_to_mapping_json/ExcelToMappingJsonApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.ccpaid.reports.excel_to_mapping_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelToMappingJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelToMappingJsonApplication.class, args);
+	}
+
+}
---
+++ excel_to_mapping_json/src/test/java/com/walmart/ccpaid/reports/excel_to_mapping_json/ExcelToMappingJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.ccpaid.reports.excel_to_mapping_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelToMappingJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
